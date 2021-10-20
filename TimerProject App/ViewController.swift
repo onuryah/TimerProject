b9b354870d905f12ad2a01004ea04a6516f3ea2b
Onuralp <xonuraralpx@gmail.com>
2021-10-20 15:23:41 +0300

    The app is counting down from ten to zero

--- TimerProject App/ViewController.swift
+++ TimerProject App/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    var timer = Timer()
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counter = 10
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+    }
+    @objc func timerFunction() {
+        timeLabel.text = "\(counter)"
+        if counter == 0{
+            timer.invalidate()
+            timeLabel.text = "Time is over"
+        }
+            counter -= 1
+        
+        
+        
     }
 
-
+    @IBAction func bottonClicked(_ sender: Any) {
+    
+    }
 }
 
